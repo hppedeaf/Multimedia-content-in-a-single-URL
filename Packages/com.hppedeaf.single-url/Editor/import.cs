8739d79a1d180447728a6204e0ec8bae6ddf5dc6
hppedeaf <eddy77310@gmail.com>
2024-09-01 20:43:52 +0200

    update

--- Packages/com.hppedeaf.single-url/Editor/import.cs
+++ Packages/com.hppedeaf.single-url/Editor/import.cs
@@ -8,11 +8,12 @@ public class ImportUnityPackage : Editor
     static void ImportPackages()
     {
         // Ensure the directory path is correct relative to the project's root
-        string[] packages = Directory.GetFiles("Packages/com.hppedeaf.single-url/", "*.unitypackage", SearchOption.AllDirectories);
+        string[] packages = Directory.GetFiles("Packages/com.hppedeaf.single-url/Runtime", "*.unitypackage", SearchOption.AllDirectories);
 
         foreach (string package in packages)
         {
             AssetDatabase.ImportPackage(package, false); // The 'false' parameter avoids the import dialog
+            Debug.Log("Imported package: " + package);
         }
 
         // Refresh the AssetDatabase after all packages are imported
@@ -30,9 +31,14 @@ public class ImportUnityPackage : Editor
         if (File.Exists(scriptFilePath))
         {
             File.Delete(scriptFilePath);
+            Debug.Log("Deleted script: " + scriptFilePath);
 
             // After deletion, force Unity to refresh the AssetDatabase to remove the script from the project
             AssetDatabase.Refresh();
+        }
+        else
+        {
+            Debug.LogWarning("Script file not found for deletion: " + scriptFilePath);
         }
     }
 }
